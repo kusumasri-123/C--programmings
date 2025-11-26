fc99b272155d8b215e0c2942b1f77224d98d45cc
kusumasri-123 <kusumasri1228@gmail.com>
2025-11-26 15:50:30 +0530

    factorial of a given number without recursion.c

---
+++ factorial of a given number without recursion.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void factorial(int);
+int main()
+{
+	int n;
+	printf("enter n value:");
+	scanf("%d",&n);
+	factorial(n);
+	return 0;
+}
+void factorial(int n)
+{
+	int i,f=1;
+	for(i=2;i<=n;i++)
+	f=f*i;
+	printf("%d!=%d\n",n,f);
+	
+}
